3f4d66bea0915b8c0a35bd6ab38ba158cadee77b
Morgan Espindola <morgan.espindola@gmail.com>
2020-05-24 14:49:49 -0300

    Created Error Handler

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -4,6 +4,8 @@ import { Injectable } from "@angular/core";
 import { MEAT_API } from "app/app.api";
 import { Observable } from "rxjs/Observable";
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch'
+import { ErrorHandler } from "app/app.error.handler";
 
 @Injectable()
 export class RestaurantsService {
@@ -12,6 +14,7 @@ export class RestaurantsService {
 
     getRestaurants(): Observable<Restaurant[]> {
         return this.http.get(`${MEAT_API}/restaurants`)
-        .map(response => response.json());
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError);
     }
 }
