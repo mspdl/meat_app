536765ce4c5941781e239bbac90474ecb0496d97
Morgan Espindola <morgan.espindola@gmail.com>
2020-06-09 21:59:51 -0300

    Lazy Loading - About Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,31 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, LOCALE_ID } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
+import { FormBuilder, FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
-import { FormsModule, ReactiveFormsModule, FormBuilder } from '@angular/forms';
-
-import { ROUTES } from './app.routes';
-
 import { AppComponent } from './app.component';
+import { ROUTES } from './app.routes';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import { RestaurantsComponent } from './restaurants/restaurants.component';
-import { RestaurantComponent } from './restaurants/restaurant/restaurant.component'
-import { RestaurantsService } from './restaurants/restaurants.service';
-import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
-import { MenuComponent } from './restaurant-detail/menu/menu.component';
-import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
-import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
-import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
-import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
+import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
+import { OrderItemsComponent } from './order/order-items/order-items.component';
+import { OrderSummaryComponent } from './order/order-summary/order-summary.component';
 import { OrderComponent } from './order/order.component';
+import { OrderService } from './order/order.service';
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
+import { ShoppingCartService } from './restaurant-detail/shopping-cart/shopping-cart.service';
+import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { RestaurantsService } from './restaurants/restaurants.service';
 import { InputComponent } from './shared/input/input.component';
 import { RadioComponent } from './shared/radio/radio.component';
-import { OrderItemsComponent } from './order/order-items/order-items.component';
-import { OrderService } from './order/order.service';
-import { DeliveryCostsComponent } from './order/delivery-costs/delivery-costs.component';
-import { OrderSummaryComponent } from './order/order-summary/order-summary.component';
 import { RatingComponent } from './shared/rating/rating.component';
+
+
 
 
 @NgModule({
@@ -34,7 +33,6 @@ import { RatingComponent } from './shared/rating/rating.component';
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    AboutComponent,
     RestaurantsComponent,
     RestaurantComponent,
     RestaurantDetailComponent,
