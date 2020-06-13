7bd1fe94033fe4f3f10bc59d86c987fc9e0be76c
Morgan Espindola <morgan.espindola@gmail.com>
2020-06-13 16:10:09 -0300

    Animations with Keyframes

--- src/app/restaurant-detail/menu-item/menu-item.component.ts
+++ src/app/restaurant-detail/menu-item/menu-item.component.ts
@@ -1,11 +1,23 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { MenuItem } from './menu-item.model';
 
 @Component({
   selector: 'mt-menu-item',
-  templateUrl: './menu-item.component.html'
+  templateUrl: './menu-item.component.html',
+  animations: [
+    trigger('menuItemAppeared', [
+      state('ready', style({ opacity: 1 })),
+      transition('void => ready', [
+        style({ opacity: 0, transform: 'translateY(-20px)' }),
+        animate('500ms 0s ease-in')
+      ])
+    ])
+  ]
 })
 export class MenuItemComponent implements OnInit {
+
+  menuItemState = 'ready'
 
   @Input() menuItem: MenuItem;
   @Output() add = new EventEmitter();
