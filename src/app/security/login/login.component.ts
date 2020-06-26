c9e8af2c3ad4a996b85ed2554ccdf930185a901f
Morgan Espindola <morgan.espindola@gmail.com>
2020-06-25 21:44:34 -0300

    Securing the Order component with CanActivate
    Securing the Order component with CanActivate

--- src/app/security/login/login.component.ts
+++ src/app/security/login/login.component.ts
@@ -27,7 +27,7 @@ export class LoginComponent implements OnInit {
       email: this.fb.control('', [Validators.required, Validators.email]),
       password: this.fb.control('', [Validators.required])
     })
-    this.navigateTo = this.activatedRoute.snapshot.params['to'] || '/'
+    this.navigateTo = this.activatedRoute.snapshot.params['to'] || btoa('/')
   }
 
   login() {
@@ -36,7 +36,7 @@ export class LoginComponent implements OnInit {
       this.loginForm.value.password)
       .subscribe(user => this.notificationService.notify(`Bem vindo, ${user.name}.`),
         response => this.notificationService.notify(response.error.message),
-        () => { this.router.navigate([this.navigateTo]) })
+        () => { this.router.navigate([atob(this.navigateTo)]) })
   }
 
 }
